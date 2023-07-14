1899243f06c0bd86a3d261a11f16edbae94ad28e
R8nDr0p <rainchrstn@gmail.com>
2023-07-14 23:49:32 +0800

    added 4 cols finished the first 3 and made it responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,63 @@
+import pic1 from "../assets/Grad_pic.jpg";
+import { BsGithub, BsFacebook, BsInstagram } from "react-icons/bs";
+
 function Hero() {
   return (
     <>
-      <section>
-        <div className="h-screen flex justify-center items-center">
-          <h1 className="text-3xl">
-            Hi! I am &nbsp;
-            <span className="font-[poppins] text-blue-500 text-4xl">
-              Rain Seguban &nbsp;
-            </span>
-            a Full Stack Web Developer
-          </h1>
-          <img src="./Grad_pic.jpg" alt="" />
-        </div>
-        <div>
-          <ul>
-            <li></li>
-          </ul>
+      <section className="bg-slate-800">
+        <div className="container mx-auto p-4">
+          <div className="row flex flex-wrap items-center sm:flex-nowrap sm:flex-row gap-1 ">
+            {/* Social Media */}
+            <div className="col flex-1">
+              <div className="row flex justify-center sm:flex-wrap gap-6 me-3 ">
+                <BsGithub className="text-5xl" />
+                <BsFacebook className="text-5xl" />
+                <BsInstagram className="text-5xl" />
+              </div>
+            </div>
+            {/* Name and Info */}
+            <div className="col flex-1 basis-full">
+              <div className="row">
+                <h1 className="text-6xl text-blue-200">
+                  Hi! I am
+                  <br />
+                  <span className="font-[poppins] text-blue-400 text-8xl">
+                    Rain Christian Seguban
+                  </span>
+                  <br />a Full Stack Web Developer
+                </h1>
+                <p className="text-2xl text-blue-200 mt-9">
+                  I'm a Junior Full Stack Web Developer with a passion for
+                  creating dynamic and user-friendly web applications.
+                  Proficient in <span className="text-blue-500">HTML</span>,{" "}
+                  <span className="text-blue-500">CSS</span>,{" "}
+                  <span className="text-blue-500">JavaScript</span>,
+                  <span className="text-blue-500">ReactJS</span>,{" "}
+                  <span className="text-blue-500">BootstrapCSS</span>,{" "}
+                  <span className="text-blue-500">TailwindCSS</span>,{" "}
+                  <span className="text-blue-500">Laravel</span>,{" "}
+                  <span className="text-blue-500">NodeJS</span>, and{" "}
+                  <span className="text-blue-500">Git</span>, I deliver
+                  high-quality code and intuitive user experiences. Let's bring
+                  your digital vision to life!
+                </p>
+                <br />
+                <div className="flex justify-center">
+                  <button className="btn my-3 py-2 px-4 rounded text-2xl text-blue-100 bg-cyan-700 shadow-lg shadow-cyan-500/25 hover:text-white hover:shadow-cyan-500/100 hover:scale-125 transition duration-200">
+                    See my projects here!
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col flex-1 basis-full">
+              <div className="row">
+                <div className="col">
+                  <img src={pic1} alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="col flex-1 basis-[1/4]">email</div>
+          </div>
         </div>
       </section>
     </>
