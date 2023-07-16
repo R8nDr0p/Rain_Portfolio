de6ce52a5eb228f3b480bbf87b8db9f3f986f6b4
R8nDr0p <rainchrstn@gmail.com>
2023-07-16 15:46:11 +0800

    added project section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Projects() {
+  const slides = [
+    {
+      url: "https://cdn.pixabay.com/photo/2013/07/12/15/03/clouds-149344_1280.png",
+    },
+  ];
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div className="row">
+          <div className="col"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
