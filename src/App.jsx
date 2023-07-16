a91ee45b334473753694f5d2608153d0e7eedfcd
R8nDr0p <rainchrstn@gmail.com>
2023-07-16 20:46:22 +0800

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <About />
       <Projects />
+      <Contact />
       <Footer />
     </>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div className="row">
+          <div className="col">
+            <h1>Contact me:</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
