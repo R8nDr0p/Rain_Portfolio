f613fcf2deb1ec0ac64a0c550d25d4a1ef2c4d5e
R8nDr0p <rainchrstn@gmail.com>
2023-07-16 10:41:42 +0800

    removed routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Layout from "./components/Layout";
 import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />} path="/">
-            <Route index element={<Hero />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Footer />
     </>
   );
 }
