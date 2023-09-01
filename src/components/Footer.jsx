f8827ad7ced8af33f56d3ecd77b145838e51d40f
R8nDr0p <rainchrstn@gmail.com>
2023-09-01 10:29:30 +0800

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 function Footer() {
   return (
-    <footer>
-      <h1>footer</h1>
+    <footer className="bg-blue-100">
+      <div className="container mx-auto">
+        <div className="row flex flex-row justify-between">
+          <div className="col flex items-center text-3xl">
+            <h1 className="font-bold">
+              Rain | <span className="font-normal">Portfolio</span>
+            </h1>
+          </div>
+          <div className="col">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="#skills-section">Skills</a>
+              </li>
+              <li>
+                <a href="#projects-section">Projects</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
