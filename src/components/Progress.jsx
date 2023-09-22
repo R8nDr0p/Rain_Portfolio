8889ed6b0ed7b89c9dd041cd25e4115278266e51
R8nDr0p <rainchrstn@gmail.com>
2023-09-22 14:52:28 +0800

    added progress bar element

---
+++ src/components/Progress.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Progress() {
+
+    return (
+        <div class="skill">
+            <span class="skill-label">Coding</span>
+            <div class="progress-bar">
+                <div class="progress" id="coding-progress"></div>
+            </div>
+        </div>
+    )
+}
+export default Progress
